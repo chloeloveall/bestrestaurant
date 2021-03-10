e3d98e4eb16ad62177f23a1c5f192c0b4bcb49d5
Ashley Porter <porter.ashley13@gmail.com>
2021-03-10 11:40:23 -0800

    change database table name, remove underscore, credit to Chloe
    Co-authored-by: Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>

--- BestRestaurant/Models/Restaurant.cs
+++ BestRestaurant/Models/Restaurant.cs
@@ -5,7 +5,7 @@ namespace BestRestaurant.Models
     public int RestaurantId { get; set; }
     public string Name { get; set; }
     public string Address { get; set; }
-    public int Phone_Number { get; set; }
+    public int PhoneNumber { get; set; }
     public int CuisineId { get; set; }
     public virtual Cuisine Cuisine { get; set; }
   }
